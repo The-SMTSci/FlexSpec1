97717674a8e40d3d69c584a8733fb6abe9a71b29
dxwayne <dxwayne@gmail.com>
2022-10-30 10:42:46 -0600

    Beefed up FlexCRC, test cases examples, rounting

--- Code/FlexSpec/UserInterface/FlexCRC.py
+++ Code/FlexSpec/UserInterface/FlexCRC.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
+# (wg-astroconda3-pdb)      # CONDA Python3
 # (wg-python-fix-pdbrc)
 #
 # (compile (format "python -m py_compile %s" (buffer-file-name)))
 ### HEREHEREHERE
 
-import os
 import sys
-import re
 import zlib
 import json
-
 
 #############################################################################
 #
@@ -32,17 +30,18 @@ import json
 #     @staticmethod
 #     def __format__(e):
 # class FlexCRC(object):
-#     #__slots__ = [''] # add legal instance variables
-#     def __init__(self,payload):                             # FlexCRC::__init__()
-#     def add(self,charstr):                                  # FlexCRC::add()
-#     def crc(self):                                          # FlexCRC::crc()
+#     def __init__(self,payload : str = ""):                  # FlexCRC::__init__()
+#     def add(self, charstr : (str)) -> 'self':               # FlexCRC::add()
+#     def crc(self,genstr=None) -> (str,bytes):                  # FlexCRC::crc()
+#     def debug(self,msg="",skip=[],os=sys.stderr) -> 'FlexCRC': # FlexCRC::debug()
 #     @staticmethod
-#     def calccrc(payload : str):
-#     def debug(self,msg="",skip=[],os=sys.stderr):           # FlexCRC::debug()
+#     def calccrc(payload : str) -> bytes:
 # if __name__ == "__main__":
+#     def traverse(i,kk,json):
 #
 #
 #
+# 2022-10-30T06:42:46-0600 wlg
 #############################################################################
 __doc__ = """
 
@@ -53,10 +52,42 @@ Tie to the CRC generation/check in the FlexSpec C++ code.
 
 """
 
-
 __author__  = 'Wayne Green'
 __version__ = '0.1'
 __all__     = ['FlexCRC','FlexCRCException']   # list of quoted items to export
+
+##############################################################################
+# testjson - from the local pier15.building3.example.com, to the
+#   remote sit at "Haydrians Pub" to the device "flexspec_rodda"
+#   (Arduino's name) with several things to dispatch there.  the
+#   receipt return path is :
+#     (Nano 33)"flexspec_rodda" -> (Rpi)"Haydrians Pub" -> (Rpi)pier15.building3.example.com
+##############################################################################
+testjson={"pier15.building3.example.com" :
+    { "Haydrians Pub":
+       {"flexspec_rodda":
+           { "ovioslit"    : {"process" : {"slit" : "20","illuminator" : "0","receipt" : "1"}},
+             "grating"     : {"process" : {"grating" : "300l/mm","cwave" : "5000","home" : "0","receipt" : "1"}},
+             "ovioslit"    : {"process" : {"slit" : "70","illuminator" : "0","receipt" : "1"}},
+             "ovioslit"    : {"process" : {"slit" : "70","illuminator" : "1","receipt" : "1"}},
+             "grating"     : {"process" : {"grating" : "600l/mm","cwave" : "5544","home" : "0","receipt" : "1"}},
+             "guider"      : {"process" : {"position" : "0.4568","direction" : "0","speed" : "0.32","home" : "0","receipt" : "1"}},
+             "guider"      : {"process" : {"position" : "0.4568","direction" : "0","speed" : "0.32","home" : "1","receipt" : "1"}},
+             "guider"      : {"process" : {"position" : "0.4600","direction" : "1","speed" : "0.32","home" : "0","receipt" : "1"}},
+             "collimator"  : {"process" : {"position" : "0.3390","direction" : "0","speed" : "0.1","home" : "0","receipt" : "1"}},
+             "collimator"  : {"process" : {"position" : "0.3380","direction" : "0","speed" : "0.1","home" : "0","receipt" : "1"}},
+             "collimator"  : {"process" : {"position" : "0.3390","direction" : "1","speed" : "0.1","home" : "0","receipt" : "1"}},
+             "collimator"  : {"process" : {"position" : "0.3390","direction" : "1","speed" : "0.1","home" : "0","receipt" : "1"}},
+             "camerafocus" : {"process" : {"camerafocus" : "0","receipt" : "1"}},
+             "camerafocus" : {"process" : {"camerafocus" : "0","receipt" : "1"}},
+             "imu"         : {"process" : {"read" : "0","home" : "1","pangle" : "59.594","receipt" : "1"}},
+             "imu"         : {"process" : {"read" : "1","home" : "0","pangle" : "39.9","receipt" : "1"}},
+           }
+       }
+    }
+}
+
+testjsonstring="""{"pier15.building3.example.com": {"Haydrians Pub": {"flexspec_rodda": {"ovioslit": {"process": {"slit": "70", "illuminator": "1", "receipt": "1"}}, "grating": {"process": {"grating": "600l/mm", "cwave": "5544", "home": "0", "receipt": "1"}}, "guider": {"process": {"position": "0.4600", "direction": "1", "speed": "0.32", "home": "0", "receipt": "1"}}, "collimator": {"process": {"position": "0.3390", "direction": "1", "speed": "0.1", "home": "0", "receipt": "1"}}, "camerafocus": {"process": {"camerafocus": "0", "receipt": "1"}}, "imu": {"process": {"read": "1", "home": "0", "pangle": "39.9", "receipt": "1"}}}}}}"""
 
 
 ##############################################################################
@@ -75,44 +106,83 @@ class FlexCRCException(Exception):
 
 
 ##############################################################################
-# FlexCRC
-#
+# FlexCRC - Given a JSON string payload, consistent with the FlexSpec
+#   protocol; compute the CRC.
 ##############################################################################
 class FlexCRC(object):
     """ Make a CRC for a given string.
         May call static method calccrc(str) without needing an instance.
     """
 
-    def __init__(self,payload):                             # FlexCRC::__init__()
-        """Initialize this class."""
+    def __init__(self, payload : str = ""):                  # FlexCRC::__init__()
+        """FlexCRC Usages:
+           Use a transient instance to grab a CRC
+              FlexCRC('{"kzing" : {"halpha" : 1}}').crc()
+           Make a non-transient instance to grab a CRC but keep c as instance!
+              (c := FlexCRC('{"my spec" : {"kzin" : {"halpha" : 1}}}').crc()
+           Start an instance and add as we go:
+              c = FlexCRC('{"my spec" : ')
+              c.add('{"kzin" : {"halpha" : 1}}')
+              c.add(myBokehObj.payload)
+              crc = c.add('}').crc
+              ...
+        """
         if(not isinstance(payload,str)):
             raise FlexCRCException("Object is not string")
-        self._payload = payload
-        self._rawcrc = payload.encode() # convert to bytes
-        self._crc    = zlib.crc32(self._rawcrc)
-        self._crcstring = ("%08X" % self._crc).encode()
+        self._payload         = payload
+        self._payloadbytes    = None        # fill in with self.crc()
+        self._crc             = None
+        self._crcstring       = None
+        self._crcbytes        = None
+        _                     = self.crc()  # side-effect set above member vars
 
     ### FlexCRC.__init__()
 
-    def add(self,charstr):                                  # FlexCRC::add()
-        """Add a character or string to the mix"""
+    def add(self, charstr : (str)) -> 'self':               # FlexCRC::add()
+        """Add a character or string to the mix.
+        Raises TypeError if bytes are added.
+        """
         self._payload += charstr
+
         return self
+
     ### FlexCRC::add()
 
-    def crc(self):                                          # FlexCRC::crc()
-        """Class member, return hex string with the payload"""
+    def crc(self,genstr=None) -> (str,bytes):                  # FlexCRC::crc()
+        """Class member, return hex string with the payload.
+        If genstr is provided, set the internal payload to this
+        value and return its crc.
 
-        return ("%08X" % zlib.crc32(self._payload.encode())).encode()
+        b   = inst.crc() gets the bytes        image  bytes*8
+        _,s = inst.crc() gets the string       image  8*utf-8 chars
+        b,s = inst.crc() gets bytes and string images
+
+        8 utf-8 characters, representing values of each nybble
+        of an CRC32 checksum presented in Big Endian format.
+        Zero padded for case len < 28 bits when that it happens.
+        zlib.crc32(bytes([0])) = 3523407757 log(3523407757)/log(2) 31.7  bits
+        zlib.crc32(bytes([ord('c')])) log(112844655 )/log(2) 26.75 bits
+        payload = "hello neurotic world"
+        crcstr = "%08X" % zlib.crc32(payload.encode())
+        payload = ""
+        "%08X" % zlib.crc32(payload.encode()) "00000000"
+        zlib.crc32(payload.encode()) = 0
+
+        """
+        # magic mumble to make that happen. f-strings no hex!
+        if(genstr is not None):
+            self._payload = genstr                  # pick up with local value.
+        payload               = self._payload                         # payload's string image
+        self._payloadbytes    = payload.encode()                # payload's bytes image
+        self._crc             = zlib.crc32(self._payloadbytes)  # its (current) crc as int
+        self._crcstring       = ("%08X" % self._crc)
+        self._crcbytes        = ("%08X" % self._crc).encode() # as a string
+
+        return (self._crcbytes,self._crcstring) # bytes,_ = x.crc() does trick.
 
     ### FlexCRC::crc()
 
-    @staticmethod
-    def calccrc(payload : str):
-        """Make a CRC from the string without an instance"""
-        return ("%08X" % zlib.crc32(payload.encode())).encode()
-
-    def debug(self,msg="",skip=[],os=sys.stderr):           # FlexCRC::debug()
+    def debug(self,msg="",skip=[],os=sys.stderr) -> 'FlexCRC': # FlexCRC::debug()
         """Help with momentary debugging, file to fit.
            msg  -- special tag for this call
            skip -- the member variables to ignore
@@ -129,12 +199,15 @@ class FlexCRC(object):
 
     ### FlexCRC.debug()
 
-    __FlexCRC_debug = debug  # really preserve our debug name if we're inherited
+    @staticmethod
+    def calccrc(payload : str) -> bytes:
+        """Make a CRC from the string without an instance"""
 
-   # (wg-python-properties properties)
+        return ("%08X" % zlib.crc32(payload.encode())).encode()
+
+    ### FlexCRC::calccrc() static.
 
 # class FlexCRC
-
 
 ##############################################################################
 #                                    Main
@@ -142,6 +215,7 @@ class FlexCRC(object):
 ##############################################################################
 # HEREHEREHERE
 if __name__ == "__main__":
+    import optparse
     opts = optparse.OptionParser(usage="%prog "+__doc__)
 
     opts.add_option("-v", "--verbose", action="store_true", dest="verboseflag",
@@ -149,10 +223,42 @@ if __name__ == "__main__":
                    help="<bool>     be verbose about work.")
 
     (options, args) = opts.parse_args()
+    import io
+    import pprint
 
-    # (wg-python-atfiles)
-    gen = FlexCRC("hello there")
+if(1):
+    def traverse(i,kk,json):
+        """Walk the test tree"""
+        for k,v in json.items():
+            #print(type(v))
+            qk = k
+            if(' ' in k):
+                qk = f"'{k}'"
+            if(isinstance(v,type({}))):
+                if(kk == ''):
+                    traverse(i+1,f"{qk}", v)
+                else:
+                    traverse(i+1,f"{kk} -> {qk}", v)
+                #print(f"{i:3d} {k} = ",end='')
+                #pprint.pprint(v,indent=4)
+                #print()
+            else:
+                print(f"  {kk} {k} = {v}",end='\n')
+    print("Dispatch routes for test:")
+    js = json.dumps(testjson)
+    print(f"testjson {testjson}")
+    traverse(1,'',testjson)
+
+
+    gen = FlexCRC(testjsonstring)
     gen.debug()
-    print(f"gen'ed crc {gen.crc()}")
+    bs,st = gen.crc()
+    print(f"gen'ed crc bytes={bs} string={st}")
 
+    print("\nbinary message:")
+    RS  = bytes([30])
+    SOH = bytes([1])
+    STX = bytes([2])
+    ETX = bytes([3])
+    print(f"""{RS}{SOH}{bs}{STX}{testjsonstring}{ETX}""")
 
