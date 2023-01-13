469529e95dedc90d814a58a36cdf260e51eb1ee3
dxwayne <dxwayne@gmail.com>
2023-01-13 09:52:03 -0700

    Adding additional steps

--- Code/rpi/FollowMe.sh
+++ Code/rpi/FollowMe.sh
@@ -26,7 +26,7 @@ sudo apt-get upgrade                          # N - keep the provider's /etc/apt
 sudo apt-get dist-upgrade
 # (accumulate a) list of packages to make sure we have.
 flexpackages   = ("openssh-server" "linux-modules-extra-raspi" "net-tools" \
-                  "curl" "gawk" "vim" "minicom" "git" "locate" "libx11-dev" \
+                  "gh" "curl" "gawk" "vim" "minicom" "git" "locate" "libx11-dev" \
                   "zlib1g-dev" "libxml2-dev" "libxslt1-dev" "autoconf" "swig" \
                   "python3-dev" "python3-pip" "python3-virtualenv" "sqlite3" \
                   "pip" "sqlitebrowser" "supervisor" "samba" "samba-tools"\
@@ -39,8 +39,8 @@ pythonpackages = ( "numpy" "scipy" "pandas" "matplotlib" "bokeh" "pandas" \
                    "astropy" "gunicorn" "pysqlite3" "xpa")
 
 usage   { echo "This script is located in a github repo:";
-          echo "/home/git/FlexSpec1/Code/rpi/FollowMe.sh";
-          echo "As root: ";
+          echo "/FlexSpec1/Code/rpi/FollowMe.sh";
+          echo "Run as root: ";
           echo "mkdir -p /home/git";
           echo "apt install -y git";
           echo "cd /home/git";
@@ -52,6 +52,7 @@ usage   { echo "This script is located in a github repo:";
               echo $(cat /sys/firmware/devicetree/base/model);
           fi
 }
+
 #############################################################################
 # The real work as root.
 #############################################################################
@@ -60,7 +61,7 @@ usage   { echo "This script is located in a github repo:";
 # https://github.com/cli/cli/blob/trunk/docs/install_linux.md
 if [[ "$(which git)" == "" ]] ; then
     apt install git -y;
-    type -p curl >/dev/null || apt install curl -y
+    type -p curl >/dev/null || apt install curl -y  # insgall curl as needed.
     curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg && \
         chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg && \
         echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null     && \
@@ -78,9 +79,8 @@ git clone https://github.com/The-SMTSci/FlexSpec1.git
 #hostnamectl set-hostname pier15          # force hostname
 
 # add a special user, all system configuration activity here.
-useradd -m -d /home/flex -G dialout -p "$(openssl passwd -1 'happy startrails')" flex
-
 export FLEXUSER=flex
+useradd -m -d /home/$FLEXUSER -G dialout -p "$(openssl passwd -1 'happy startrails')" $FLEXUSER
 
 usermod -aG dialout $FLEXUSER     # allow user 'flex' ability to use facilities
 usermod -aG tty     $FLEXUSER
@@ -102,7 +102,7 @@ Github:
   - 
 EOF1
 
-
+######################### Initial Batch of Installs #########################
 
 apt     install -y ufw                       # uncomplicated firewall
 apt     install -y openssh-server            # add openssh capability
@@ -124,7 +124,7 @@ fi
 
 apt     install -y minicom                   # because,,, handy interface to serial
 
-# Load these packages now, get it over with for ds9.
+# Load these packages now, get it over with for local compiles.
 # Handy anyway.
 apt     install -y locate                    # bacause,,, locate!
 apt     install -y build-essential           # get the compilers...
@@ -139,12 +139,14 @@ apt     install -y swig
 apt     install -y python3-dev python3-pip python3-virtualenv
 
 #############################################################################
-# fixup .bashrc
+# Grab the initialization scripts,files and data from FlexSpec needs....
+# fixup $FLEXUSER/.bashrc
 #############################################################################
 cd ~/git                                     # get the FlexSpec and install
 git clone https://github.com/The-SMTSci/FlexSpec1.git
+export ANCHOR=/home/$FLEXUSER/git/FlexSpec1
 cd ~/git/FlexSpec1/Code/HOME
-cp pi.aliases ~/.pi.aliases
+cp pi.aliases ~/.pi.aliases                  # handy aliases
 cp vimrc      ~/.vimrc
 cp vimrc      /root                          # add a decent vimrc for sudo
 mkdir -p /var/www/html/FlexSpec1
@@ -175,7 +177,7 @@ pip install astropy
 pip install gunicorn
 pip install pysqlite3
 apt install -y sqlite3 sqlitebrowser
-apt install -y supervisor
+apt install -y supervisor                   # easily manage our servers
 
 #############################################################################
 # Load up Kstars/Ekos/libindi and drivers.
@@ -191,18 +193,17 @@ apt     install  -y gsc
 # DIFS is Common Internet Fileshares
 #############################################################################
 apt install -y samba samba-tools smbclient cifs-utils
-systemctl enable --now smbd                  # retister for all reboots
+systemctl enable --now smbd                  # register for all reboots
 
-usermod -aG sambashare flex
-smbpasswd -a "flex%time has come"
+usermod -aG sambashare $FLEXUSER             # let $FLEXUSER share with smb.                  
+smbpasswd -a "flex%time has come"            # initial password...
 mkdir -p /samba/{$FLEXUSER,flex}             # make shares for the two main users
 chgrp -R sambashare /samba
-usermod -aG sambashare $FLEXUSER             # add these users to group
-usermod -aG sambashare flex
+
 #smb://winhost/shared-folder-name
 # TODO mod /etc/samba/smb.conf
-systemctl restart smbd
-systemctl restart nmbd
+systemctl restart smbd                       # Start the SMB service
+systemctl restart nmbd                       # Microsoft NETBIOS stuff
 
 #############################################################################
 # NFS - for other linux like clients
@@ -212,7 +213,7 @@ mkdir -p /mnt/share
 chown -R nobody:nogroup /mnt/share/
 chmod 777 /mnt/share/
 exportfs -a
-systemctl restart nfs-kernel-server
+systemctl restart nfs-kernel-server          # start nfs
 
 # add daemons
 # cp ANCHOR/bokeh/bokeh.service /etc/systemd/system/bokeh.service
@@ -220,45 +221,40 @@ systemctl restart nfs-kernel-server
 # systemctl daemon-reload
 # systemctl enable bokeh.service
 
+#############################################################################
+# Install and configure nginx
+#   apt install nginx
+#   cp the files from FlexSpec repo into place
+# q
+#############################################################################
+
 apt install -y nginx                         # nginx local access to bokeh
 apt install -y apache2-utils                 # for auth with usernames and passwords
-# create default passwords for $FLEXUSER and our flex default user.
-htpasswd -cb /etc/nginx/.htpasswd $FLEXUSER "PwD4$USER"
+# create default nginx passwords for $FLEXUSER and our flex default user.
+htpasswd -cb /etc/nginx/.htpasswd $FLEXUSER "PwD4$FLEXUSER"
 htpasswd -b  /etc/nginx/.htpasswd flex  "Flex"  # TODO  make more secure.
 
-mkdir -p /var/log/nginx/flexspec/
+mkdir -p /var/log/nginx/flexspec/            # create a logfile directory
 # create access and error logs for our instrument in usual place.
-touch /var/log/nginx/flexspec/{access,error}.log
+touch /var/log/nginx/flexspec/{access,error}.log # and handy files for nginx
 # install our pre-backed files
-cp $ANCHOR/nginx/sites-available/flexspec /etc/nginx/sites-available/flexspec
+cp -pr $ANCHOR/FlexBerry/nginx/sites-available/flexspec/* /etc/nginx/sites-available/flexspec
+cp -pr $ANCHOR/FlexBerry/nginx/nginx.conf /etc/nginx/ # with logfile format.
+pushd /etc/nginx/sites-enabled
+ln -s ../sites-available/flexspec  flexspec       # link this in as enabled
+popd
+mkdir -p /var/www/html
+pushd /var/www/html
+mkdir FlexSpec
+ln -s FlexSpec flexspec                      # allow lowercase name maintain case sensitivity
+# make the nginx content
+echo "make nginx"                            # TODO
 
 # get it operational now.
 systemctl restart nginx
 
 # Get the CIDR net/mask
 
-netinterface=$(ip -f inet -o addr | awk -e '/e(th|no)[0-9]/ { print $2;}')
-cidrnet=$(ip -f inet -o addr | awk -e '/e(th|no)[0-9]/ { print $4;}')
-
-# ufw : 
-# /etc/ufw/before.rules
-# /etc/ufw/user.rules
-# /etc/ufw/user6.rules
-ufw allow in on $netinterface from $cidrnet
-ufw allow ssh                                        # allow ssh in the firewall
-ufw allow dns                                        # bind9
-ufw allow from $cidrnet to any port 5006 proto tcp   # Bokeh
-ufw allow from $cidrnet to any port 5006 proto udp
-ufw allow from $cidrnet to any port 7654 proto tcp   # libindi
-ufw allow from $cidrnet to any port 7654 proto udp
-ufw allow from $cidrnet to any port  443 proto tcp   # https
-ufw allow from $cidrnet to any port  443 proto udp
-ufw allow samba
-ufw allow from client_ip to any port nfs             # NFS share
-ufw allow 1194/udp                                   # VPN
-ufw allow 1194/tcp                                   # remember for next time TODO
-ufw disable
-ufw enable
 #setxkbmap us  # needed because initial install went sideways
 
 # ip -4 show $netinterface
@@ -281,13 +277,51 @@ apt install -y filezilla                     # GUI to move files between systems
 apt install -y iraf
 apt install -y python-pyraf3
 
+netinterface=$(ip -f inet -o addr | awk -e '/e(th|no)[0-9]/ { print $2;}')
+cidrnet=$(ip -f inet -o addr | awk -e '/e(th|no)[0-9]/ { print $4;}')
+
+#############################################################################
+# ufw : Brew up and activate a sloppy IPTABLES for the firewall
+# /etc/ufw/before.rules   # files of interest
+# /etc/ufw/user.rules
+# /etc/ufw/user6.rules
+#############################################################################
+ufw allow in on $netinterface from $cidrnet
+ufw allow ssh                                        # allow ssh in the firewall
+ufw allow dns                                        # bind9
+ufw allow from $cidrnet to any port 5006 proto tcp   # Bokeh
+ufw allow from $cidrnet to any port 5006 proto udp
+ufw allow from $cidrnet to any port 7654 proto tcp   # libindi
+ufw allow from $cidrnet to any port 7654 proto udp
+ufw allow from $cidrnet to any port  443 proto tcp   # https
+ufw allow from $cidrnet to any port  443 proto udp
+ufw allow from client_ip to any port samba
+ufw allow from client_ip to any port nfs             # NFS share
+ufw allow 1194/udp                                   # VPN
+ufw allow 1194/tcp                                   # remember for next time TODO
+ufw disable
+ufw enable
+
 #############################################################################
 # Netgate 100 setup: This is not on the FlexBerry RPi.
 # https://www.netgate.com/resources/videos-configuring-openvpn-remote-access-in-pfsense-software
 #############################################################################
 
-
-
+#############################################################################
+# Notes:
+#  Github has its own package "gh" and requires additional work to get a
+#    credential to use if two factor authentication has been enabled on
+#    the account.
+#
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+#############################################################################
 
 
 
